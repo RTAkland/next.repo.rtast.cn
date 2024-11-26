9a51c1e1df16106e5485920c70d76922d4fee74c
RTAkland <me@rtast.cn>
2024-11-26 15:12:00 +0800

    Update index.js

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   document.getElementById("next-switch").addEventListener("change", (e) => {
-    document.cookie = `gitlab_canary=${e.currentTarget.checked};domain=.repo.rtast.cn;path=/;expires=${new Date(Date.now() + 31536000000).toUTCString()}`;
+    document.cookie = `gitlab_canary=${e.currentTarget.checked};domain=.rtast.cn;path=/;expires=${new Date(Date.now() + 31536000000).toUTCString()}`;
     const favicon = e.currentTarget.checked ? NEXT_FAVICON : DEFAULT_FAVICON;
     setFavicon(favicon);
   });
